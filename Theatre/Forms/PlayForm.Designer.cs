18eb943bec8d2787f6ee030fcb5341d4714f0b5d
Daniel Iliev <47360939+nCodesDotEU@users.noreply.github.com>
2021-10-05 15:16:25 +0200

    Add play form

--- Theatre/Forms/PlayForm.Designer.cs
+++ Theatre/Forms/PlayForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Theatre.Forms
 {
-    partial class ProductionForm
+    partial class PlayForm
     {
         /// <summary>
         /// Required designer variable.
@@ -36,14 +36,14 @@ namespace Theatre.Forms
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -55,21 +55,21 @@ namespace Theatre.Forms
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label5.Location = new System.Drawing.Point(67, 56);
+            this.label5.Location = new System.Drawing.Point(67, 43);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 17);
-            this.label5.TabIndex = 42;
-            this.label5.Text = "Search by name";
+            this.label5.Size = new System.Drawing.Size(95, 17);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Search by date";
             // 
             // pictureBox4
             // 
             this.pictureBox4.ImageLocation = "https://img-premium.flaticon.com/png/512/200/premium/200941.png?token=exp=1633439" +
     "247~hmac=dbf8a5a21e1836a4051f19b962c5acb0";
-            this.pictureBox4.Location = new System.Drawing.Point(460, 60);
+            this.pictureBox4.Location = new System.Drawing.Point(460, 47);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(56, 56);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 41;
+            this.pictureBox4.TabIndex = 56;
             this.pictureBox4.TabStop = false;
             // 
             // textBox6
@@ -78,70 +78,48 @@ namespace Theatre.Forms
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox6.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.textBox6.Location = new System.Drawing.Point(53, 79);
+            this.textBox6.Location = new System.Drawing.Point(53, 66);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(367, 18);
-            this.textBox6.TabIndex = 40;
+            this.textBox6.Size = new System.Drawing.Size(170, 18);
+            this.textBox6.TabIndex = 55;
             // 
             // pictureBox3
             // 
             this.pictureBox3.ImageLocation = "https://cdn-icons-png.flaticon.com/512/189/189690.png";
-            this.pictureBox3.Location = new System.Drawing.Point(413, 378);
+            this.pictureBox3.Location = new System.Drawing.Point(413, 365);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(56, 56);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 39;
+            this.pictureBox3.TabIndex = 54;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.ImageLocation = "https://cdn-icons-png.flaticon.com/512/1057/1057097.png";
-            this.pictureBox2.Location = new System.Drawing.Point(273, 378);
+            this.pictureBox2.Location = new System.Drawing.Point(273, 365);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(56, 56);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabIndex = 53;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.ImageLocation = "https://cdn-icons-png.flaticon.com/512/929/929409.png";
-            this.pictureBox1.Location = new System.Drawing.Point(128, 378);
+            this.pictureBox1.Location = new System.Drawing.Point(128, 365);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(56, 56);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabIndex = 52;
             this.pictureBox1.TabStop = false;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
-            this.panel1.Location = new System.Drawing.Point(53, 323);
+            this.panel1.Location = new System.Drawing.Point(53, 310);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(495, 5);
-            this.panel1.TabIndex = 36;
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.textBox4.Location = new System.Drawing.Point(308, 257);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(240, 18);
-            this.textBox4.TabIndex = 35;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label3.Location = new System.Drawing.Point(322, 233);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 21);
-            this.label3.TabIndex = 34;
-            this.label3.Text = "DENIER";
+            this.panel1.TabIndex = 51;
             // 
             // textBox5
             // 
@@ -149,21 +127,21 @@ namespace Theatre.Forms
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox5.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.textBox5.Location = new System.Drawing.Point(53, 257);
+            this.textBox5.Location = new System.Drawing.Point(53, 244);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(220, 18);
-            this.textBox5.TabIndex = 33;
+            this.textBox5.Size = new System.Drawing.Size(495, 18);
+            this.textBox5.TabIndex = 48;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label4.Location = new System.Drawing.Point(66, 231);
+            this.label4.Location = new System.Drawing.Point(66, 218);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 21);
-            this.label4.TabIndex = 32;
-            this.label4.Text = "PREMIER";
+            this.label4.Size = new System.Drawing.Size(167, 21);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "PRODUCTION NAME";
             // 
             // textBox3
             // 
@@ -171,22 +149,22 @@ namespace Theatre.Forms
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.textBox3.Location = new System.Drawing.Point(308, 165);
+            this.textBox3.Location = new System.Drawing.Point(308, 152);
             this.textBox3.MaxLength = 9;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(240, 18);
-            this.textBox3.TabIndex = 31;
+            this.textBox3.TabIndex = 46;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label2.Location = new System.Drawing.Point(322, 141);
+            this.label2.Location = new System.Drawing.Point(322, 128);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 21);
-            this.label2.TabIndex = 30;
-            this.label2.Text = "AUTHOR";
+            this.label2.Size = new System.Drawing.Size(106, 21);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "PARTICIPATE";
             // 
             // textBox2
             // 
@@ -194,28 +172,52 @@ namespace Theatre.Forms
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.textBox2.Location = new System.Drawing.Point(53, 165);
+            this.textBox2.Location = new System.Drawing.Point(53, 152);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(220, 18);
-            this.textBox2.TabIndex = 29;
+            this.textBox2.TabIndex = 44;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
-            this.label1.Location = new System.Drawing.Point(66, 139);
+            this.label1.Location = new System.Drawing.Point(66, 126);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 21);
-            this.label1.TabIndex = 28;
-            this.label1.Text = "NAME";
+            this.label1.Size = new System.Drawing.Size(92, 21);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "PLAY DATE";
             // 
-            // ProductionForm
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.label6.Location = new System.Drawing.Point(261, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 17);
+            this.label6.TabIndex = 59;
+            this.label6.Text = "Search by name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.textBox1.Location = new System.Drawing.Point(247, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 18);
+            this.textBox1.TabIndex = 58;
+            // 
+            // PlayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
             this.ClientSize = new System.Drawing.Size(600, 465);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.textBox6);
@@ -223,8 +225,6 @@ namespace Theatre.Forms
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -232,8 +232,8 @@ namespace Theatre.Forms
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "ProductionForm";
-            this.Text = "ProductionForm";
+            this.Name = "PlayForm";
+            this.Text = "PlayForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -252,13 +252,13 @@ namespace Theatre.Forms
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
